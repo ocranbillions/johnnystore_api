ed8eb27c6cfc66412c57a8efdcf30350ecb8bd75
Samuel Ocran <sammiestt@gmail.com>
2021-03-06 11:58:22 +0100

    enable tests

--- src/controllers/__test__/integration.test.ts
+++ src/controllers/__test__/integration.test.ts
@@ -86,211 +86,211 @@ describe("AUTH/LOGIN", () => {
 
 /* ================ PRODUCTS ================ */
 
-// describe("PRODUCTS", () => {
-//   it('fetches products successfully', async () => {
-//     const res = await request(app).get('/products');
+describe("PRODUCTS", () => {
+  it('fetches products successfully', async () => {
+    const res = await request(app).get('/products');
     
-//     const { products } = res.body;
-//     const totalProducts = products.length
-//     expect(res.status).toEqual(200)
-//     expect(products).toBeTruthy()
-//     expect(products[totalProducts - 1]).toEqual({
-//       "id": 60,
-//       "name": "Pop Corn",
-//       "price": 100,
-//       "stock": 30
-//     });
-//   });
+    const { products } = res.body;
+    const totalProducts = products.length
+    expect(res.status).toEqual(200)
+    expect(products).toBeTruthy()
+    expect(products[totalProducts - 1]).toEqual({
+      "id": 60,
+      "name": "Pop Corn",
+      "price": 100,
+      "stock": 30
+    });
+  });
 
-//   it('fetches top selling products by specified dates', async () => {
-//     const from = "2019-01-01";
-//     const to = "2019-12-31";
+  it('fetches top selling products by specified dates', async () => {
+    const from = "2019-01-01";
+    const to = "2019-12-31";
 
-//     const res = await request(app).get(`/products?topselling=true&from=${from}&to=${to}`);
+    const res = await request(app).get(`/products?topselling=true&from=${from}&to=${to}`);
     
-//     const { products } = res.body;
-//     expect(res.status).toEqual(200)
-//     expect(products).toBeTruthy()
-//     expect(products[0]).toEqual({
-//       "id": 17,
-//       "name": "Chips",
-//       "price": 50,
-//       "qtySold": 41
-//     })
-//   });
-// });
+    const { products } = res.body;
+    expect(res.status).toEqual(200)
+    expect(products).toBeTruthy()
+    expect(products[0]).toEqual({
+      "id": 17,
+      "name": "Chips",
+      "price": 50,
+      "qtySold": 41
+    })
+  });
+});
 
 
-// /* ================ ORDERS ================ */
-// describe("ORDERS", () => {
-//   it('Require\'s employees to be logged in before placing order', async () => {
-//     const res = await request(app).post('/orders');
-//     const { message } = res.body;
-//     expect(res.status).toEqual(401)
-//     expect(message).toEqual('Your session expired. Please sign in!')
-//   })
+/* ================ ORDERS ================ */
+describe("ORDERS", () => {
+  it('Require\'s employees to be logged in before placing order', async () => {
+    const res = await request(app).post('/orders');
+    const { message } = res.body;
+    expect(res.status).toEqual(401)
+    expect(message).toEqual('Your session expired. Please sign in!')
+  })
 
-//   it('Validates skuId, quantity, and paidInBox on req.body', async () => {
-//     const token = await login("samuel@gmail.com", "pass123")
+  it('Validates skuId, quantity, and paidInBox on req.body', async () => {
+    const token = await login("samuel@gmail.com", "pass123")
 
-//     const res = await request(app)
-//       .post('/orders')
-//       .set('Authorization', token)
-//       .send({
-//         // skuId: 10,
-//         // quantity: 4,
-//         // paidInBox: false
-//       })
-//     const { message, errorList } = res.body;
-//     expect(res.status).toEqual(400)
-//     expect(message).toEqual('Invalid Inputs!')
-//     expect(errorList[0].skuId).toEqual('Please provide an skuId (item id)')
-//     expect(errorList[1].quantity).toEqual('Please provide the quantity')
-//     expect(errorList[2].paidInBox).toEqual('Please specify if you\'re paying in box. (true | false)')
-//   })
+    const res = await request(app)
+      .post('/orders')
+      .set('Authorization', token)
+      .send({
+        // skuId: 10,
+        // quantity: 4,
+        // paidInBox: false
+      })
+    const { message, errorList } = res.body;
+    expect(res.status).toEqual(400)
+    expect(message).toEqual('Invalid Inputs!')
+    expect(errorList[0].skuId).toEqual('Please provide an skuId (item id)')
+    expect(errorList[1].quantity).toEqual('Please provide the quantity')
+    expect(errorList[2].paidInBox).toEqual('Please specify if you\'re paying in box. (true | false)')
+  })
 
-//   it('Checks that item exist before placing order', async () => {
-//     const token = await login("samuel@gmail.com", "pass123")
+  it('Checks that item exist before placing order', async () => {
+    const token = await login("samuel@gmail.com", "pass123")
 
-//     const res = await request(app)
-//       .post('/orders')
-//       .set('Authorization', token)
-//       .send({
-//         skuId: 88001, // doesnt exist
-//         quantity: 1,
-//         paidInBox: false
-//       })
-//     const { message } = res.body;
-//     expect(res.status).toEqual(404)
-//     expect(message).toEqual('The item with the id 88001 cannot be found')
-//   })
+    const res = await request(app)
+      .post('/orders')
+      .set('Authorization', token)
+      .send({
+        skuId: 88001, // doesnt exist
+        quantity: 1,
+        paidInBox: false
+      })
+    const { message } = res.body;
+    expect(res.status).toEqual(404)
+    expect(message).toEqual('The item with the id 88001 cannot be found')
+  })
 
-//   it('Checks that item is in stock before placing order', async () => {
-//     const token = await login("samuel@gmail.com", "pass123")
+  it('Checks that item is in stock before placing order', async () => {
+    const token = await login("samuel@gmail.com", "pass123")
 
-//     const res = await request(app)
-//       .post('/orders')
-//       .set('Authorization', token)
-//       .send({
-//         skuId: 59,
-//         quantity: 2,
-//         paidInBox: true
-//       })
-//     const { message } = res.body;
-//     expect(res.status).toEqual(400)
-//     expect(message).toEqual('Sorry we\'re out of stock')
-//   })
+    const res = await request(app)
+      .post('/orders')
+      .set('Authorization', token)
+      .send({
+        skuId: 59,
+        quantity: 2,
+        paidInBox: true
+      })
+    const { message } = res.body;
+    expect(res.status).toEqual(400)
+    expect(message).toEqual('Sorry we\'re out of stock')
+  })
 
-//   it('Checks that quantity to purchase is not more than current quantity in stock', async () => {
-//     const token = await login("samuel@gmail.com", "pass123")
+  it('Checks that quantity to purchase is not more than current quantity in stock', async () => {
+    const token = await login("samuel@gmail.com", "pass123")
 
-//     const res = await request(app)
-//       .post('/orders')
-//       .set('Authorization', token)
-//       .send({
-//         skuId: 1,
-//         quantity: 50000,
-//         paidInBox: true
-//       })
-//     const { message } = res.body;
-//     expect(res.status).toEqual(400)
-//     expect(message).toEqual('Available qty is 30')
-//   })
+    const res = await request(app)
+      .post('/orders')
+      .set('Authorization', token)
+      .send({
+        skuId: 1,
+        quantity: 50000,
+        paidInBox: true
+      })
+    const { message } = res.body;
+    expect(res.status).toEqual(400)
+    expect(message).toEqual('Available qty is 30')
+  })
 
-//   it('Places order successfully and respond\s with employee\'s current debt/bill status', async () => {
-//     const token = await login("samuel@gmail.com", "pass123")
+  it('Places order successfully and respond\s with employee\'s current debt/bill status', async () => {
+    const token = await login("samuel@gmail.com", "pass123")
 
-//     const res = await request(app)
-//       .post('/orders')
-//       .set('Authorization', token)
-//       .send({
-//         skuId: 1,
-//         quantity: 1,
-//         paidInBox: true
-//       })
-//     const { message } = res.body;
-//     expect(res.status).toEqual(201)
-//     expect(message).toEqual('Transaction completed - You do not have any unpaid bills for this month')
-//   })
+    const res = await request(app)
+      .post('/orders')
+      .set('Authorization', token)
+      .send({
+        skuId: 1,
+        quantity: 1,
+        paidInBox: true
+      })
+    const { message } = res.body;
+    expect(res.status).toEqual(201)
+    expect(message).toEqual('Transaction completed - You do not have any unpaid bills for this month')
+  })
 
-//   it('Records/Updates employee debt if item was purchased on credit i.e. !paidInBox', async () => {
-//     const token = await login("samuel@gmail.com", "pass123")
+  it('Records/Updates employee debt if item was purchased on credit i.e. !paidInBox', async () => {
+    const token = await login("samuel@gmail.com", "pass123")
 
-//     const res = await request(app)
-//       .post('/orders')
-//       .set('Authorization', token)
-//       .send({
-//         skuId: 1, // price of item Id 1 = $50
-//         quantity: 3, // purchased 3 units of Fromage (skuId 1)
-//         paidInBox: false // purchased on credit
-//       })
+    const res = await request(app)
+      .post('/orders')
+      .set('Authorization', token)
+      .send({
+        skuId: 1, // price of item Id 1 = $50
+        quantity: 3, // purchased 3 units of Fromage (skuId 1)
+        paidInBox: false // purchased on credit
+      })
     
-//     const { message } = res.body;
-//     expect(res.status).toEqual(201)
-//     expect(message).toEqual('Transaction completed - Your total bill for the current month is $150')
-//   })
+    const { message } = res.body;
+    expect(res.status).toEqual(201)
+    expect(message).toEqual('Transaction completed - Your total bill for the current month is $150')
+  })
 
-//   it('Does not record the purchase as debt if employee made payment i.e. paidInBox', async () => {
-//     const token = await login("samuel@gmail.com", "pass123")
+  it('Does not record the purchase as debt if employee made payment i.e. paidInBox', async () => {
+    const token = await login("samuel@gmail.com", "pass123")
 
-//     const res = await request(app)
-//       .post('/orders')
-//       .set('Authorization', token)
-//       .send({
-//         skuId: 1, // price of item Id 1 = $50
-//         quantity: 2, // purchases two more
-//         paidInBox: true // payment made this time
-//       })
+    const res = await request(app)
+      .post('/orders')
+      .set('Authorization', token)
+      .send({
+        skuId: 1, // price of item Id 1 = $50
+        quantity: 2, // purchases two more
+        paidInBox: true // payment made this time
+      })
     
-//     const { message } = res.body;
-//     expect(res.status).toEqual(201)
-//     expect(message).toEqual('Transaction completed - Your total bill for the current month remains $150')
-//   })
-// });
+    const { message } = res.body;
+    expect(res.status).toEqual(201)
+    expect(message).toEqual('Transaction completed - Your total bill for the current month remains $150')
+  })
+});
 
 
 
-// /* ================ BILLS ================ */
-// describe("BILLS", () => {
-//   it('Returns the logged in employee\'s bill for the current month', async () => {
-//     const token = await login("samuel@gmail.com", "pass123")
+/* ================ BILLS ================ */
+describe("BILLS", () => {
+  it('Returns the logged in employee\'s bill for the current month', async () => {
+    const token = await login("samuel@gmail.com", "pass123")
 
-//     const res = await request(app)
-//     .get('/bills')
-//     .set('Authorization', token)
+    const res = await request(app)
+    .get('/bills')
+    .set('Authorization', token)
     
-//     const { message } = res.body;
-//     expect(res.status).toEqual(200)
-//     expect(message).toEqual('Your total bill for the current month is $150')
-//   });
+    const { message } = res.body;
+    expect(res.status).toEqual(200)
+    expect(message).toEqual('Your total bill for the current month is $150')
+  });
 
-//   it('Returns employee\'s bill for JohnnyStore Admin using the employee\'s email', async () => {
-//     const adminToken = await login("admin@gmail.com", "pass123")
+  it('Returns employee\'s bill for JohnnyStore Admin using the employee\'s email', async () => {
+    const adminToken = await login("admin@gmail.com", "pass123")
 
-//     const res = await request(app)
-//     .post('/bills')
-//     .set('Authorization', adminToken)
-//     .send({
-//       employeeEmail: "samuel@gmail.com"
-//     })
+    const res = await request(app)
+    .post('/bills')
+    .set('Authorization', adminToken)
+    .send({
+      employeeEmail: "samuel@gmail.com"
+    })
     
-//     const { message } = res.body;
-//     expect(res.status).toEqual(200)
-//     expect(message).toEqual('Samuel\'s total bill for the current month is $150')
-//   });
+    const { message } = res.body;
+    expect(res.status).toEqual(200)
+    expect(message).toEqual('Samuel\'s total bill for the current month is $150')
+  });
 
-//   it('Returns a 404 to Admin if the given employee isnt found when checking employee bill', async () => {
-//     const adminToken = await login("admin@gmail.com", "pass123")
+  it('Returns a 404 to Admin if the given employee isnt found when checking employee bill', async () => {
+    const adminToken = await login("admin@gmail.com", "pass123")
 
-//     const res = await request(app)
-//     .post('/bills')
-//     .set('Authorization', adminToken)
-//     .send({
-//       employeeEmail: "random@gmail.com" // non existing employee
-//     })
+    const res = await request(app)
+    .post('/bills')
+    .set('Authorization', adminToken)
+    .send({
+      employeeEmail: "random@gmail.com" // non existing employee
+    })
     
-//     const { message } = res.body;
-//     expect(res.status).toEqual(404)
-//     expect(message).toEqual('Employee not found!')
-//   });
-// });
+    const { message } = res.body;
+    expect(res.status).toEqual(404)
+    expect(message).toEqual('Employee not found!')
+  });
+});
